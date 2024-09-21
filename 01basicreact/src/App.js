90ae35426d126a9649db68db1ca3ea972ba0f6db
bilal-1235 <bilal.afsarpk55@gmail.com>
2024-09-21 16:36:59 +0500

    learn about react file structure

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -9,7 +9,18 @@ function App() {
     create react app is a utility is a high utility 
     package.json is use for info of project basically details of project 
     web vital is use for tract the performance of website 
-    npm run start (we can start the project using this Script) </>
+    npm run start "(we can start the project using this Script)"
+    Reactdom is the (virtual tree which is create by react two types of dom brower dom and reactdom(virtualdom) )  
+ReactDom have a method Which is CreateRoot and CreateRoot Provides as the doucment.getElementbyId using doucment.getElementbyId we are connect with the Html Id like id ="root"
+root.render() fuction is use for give output or process the code
+React.StrictMode is the Property of  react 
+in react we take a fuaction and in function we are use HTML tag and export it 
+in react project our main file is index.html and we inject index.js is it inaddtion we are use app.js but in vite direct inject the Script Tag 
+when we create component we use jsx instead of jx (reactbasic)
+both in vite and basicreact we write function name first letter in uppercase
+in vite we can return only one tag but we hold multiple tag in one tag using Fragment
+    
+     </>
     
   );
 }
