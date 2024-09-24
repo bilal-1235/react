08d5fabcb4ad579ecadd2e9140faed118cd28ec4
bilal-1235 <bilal.afsarpk55@gmail.com>
2024-09-24 15:46:23 +0500

    Hero Section

---
+++ portfolio-project/src/Component/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.jpg'
+
+
+const  Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt="" />
+    <h1> <span>I m Muhammad Bilal</span> , Frontend Developer based in KSA</h1>
+    <p> I am frontend Developer from KSA with 2 Years of Experience</p>
+    <div className="Hero-action">
+        <div className="hero-connect">Connect With me</div>
+        <div className="Hero-resume"> MY Resume</div>
+    </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Hero
