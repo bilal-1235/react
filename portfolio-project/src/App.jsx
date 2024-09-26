c1fad6e3c4648fdb8bf0adae9e40d745a1a32d73
bilal-1235 <bilal.afsarpk55@gmail.com>
2024-09-26 17:44:25 +0500

    add two new component

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Component/Navbar/Navbar'
 import Hero from './Component/Hero/Hero'
 import About from './Component/About/About'
+import Services from './Component/Services/Services'
+import  Mywork  from './Component/MyWork/Mywork'
 
 const  App=()=> {
   return (
@@ -9,6 +11,8 @@ const  App=()=> {
     <Navbar/>
     <Hero/>
     <About/>
+    <Services/>
+    <Mywork/>
     </div>
     
   )
