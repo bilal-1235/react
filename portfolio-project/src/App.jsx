3171aa765c9ba44a0ca34cd7506fef1801a240ac
bilal-1235 <bilal.afsarpk55@gmail.com>
2024-09-27 18:46:01 +0500

    contact section complete

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Component/Hero/Hero'
 import About from './Component/About/About'
 import Services from './Component/Services/Services'
 import  Mywork  from './Component/MyWork/Mywork'
+import Contact from './Component/Contact/Contact'
 
 const  App=()=> {
   return (
@@ -13,6 +14,7 @@ const  App=()=> {
     <About/>
     <Services/>
     <Mywork/>
+    <Contact/>
     </div>
     
   )
